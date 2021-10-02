26bf4a6f47a4381ea2d95cec740f7dc685464914
new2 <new2@wael>
2021-10-02 10:54:37 -0400

    changz

--- src/Saturday/DemoSunday.java
+++ src/Saturday/DemoSunday.java
@@ -1,12 +1,13 @@
 package Saturday;
 
 public class DemoSunday {
- public static void main (String[]args) {
+	public static void subNum(int a, int b){
+		System.out.println(a-b);
+	}
+	
+	public static void main (String[]args) {
 	System.out.println("Demo Sunday.......");
 	System.out.println("xyz");
 	System.out.println("wael");
 }
-	// coming from github
-	public static void addNum(int a, int b){
-		System.out.println(a+b);
 }
