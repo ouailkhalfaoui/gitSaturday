f8e9095d1a301ed17a552eeeb7a97f24db99cae7
new2 <new2@wael>
2021-10-02 10:41:54 -0400

    changes

--- src/Saturday/DemoSaturday.java
+++ src/Saturday/DemoSaturday.java
@@ -1,5 +1,7 @@
 package Saturday;
 
 public class DemoSaturday {
-
+	public static void main (String[]args) {
+		System.out.println("Demo Saturday.......");
+	}
 }
--- src/Saturday/DemoSunday.java
+++ src/Saturday/DemoSunday.java
@@ -1,5 +1,9 @@
 package Saturday;
 
 public class DemoSunday {
-
+ public static void main (String[]args) {
+	System.out.println("Demo Sunday.......");
+	System.out.println("xyz");
+	System.out.println("wael");
+}
 }
